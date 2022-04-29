61c86d9f9008695acef488480d38b3c2376b28f8
IanoSang <ianosang18@gmail.com>
2022-04-30 01:00:22 +0300

    feat: Sources-adds a sources class to models that will give parameters to be obtained in the sources part of the api

--- app/__init__.py
+++ app/__init__.py
@@ -11,3 +11,14 @@ def create_app(config_name):
     # Will add the views and forms
 
     return app
+
+
+class Config:
+    """
+    General configuration parent class
+    """
+    NEWS_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
+
+
+def app():
+    return None
---
+++ app/requests.py
@@ -0,0 +1,4 @@
+from app import app
+import urllib.request, json
+from .models import Sources
+
