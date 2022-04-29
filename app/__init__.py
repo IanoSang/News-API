0f9dd2beacd1ba98c0fa2e917fa29855bc4e71db
IanoSang <ianosang18@gmail.com>
2022-04-29 23:29:14 +0300

    feat: adds all directories in the app

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from config import config_options
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Will add the views and forms
+
+    return app
---
+++ app/main/views.py
@@ -0,0 +1,3 @@
+from flask import render_template, request, redirect, url_for
+from . import main
+
