15db5c91cdea0bbaad77156a61f816b523135c99
IanoSang <ianosang18@gmail.com>
2022-05-03 11:51:06 +0300

    feat: views- adds articles views page function that returns the  article details page and its data

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from . import main
-from ..requests import get_sources
+from ..requests import get_sources, get_articles
 
 
 @main.route('/')
@@ -13,3 +13,13 @@ def index():
     title = 'News Sources'
 
     return render_template('index.html', title=title, sources=sources)
+
+
+@main.route('/articles/<sources_id>')
+def articles(sources_id):
+    """
+    View articles page function that returns the  article details page and its data
+    """
+    articles = get_articles(sources_id)
+
+    return render_template('articles.html', articles=articles)
